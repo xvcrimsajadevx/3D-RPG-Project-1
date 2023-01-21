455465cb9a337cc8c5fc2c93a1742bf02ca408eb
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-01-21 01:57:32 -0700

    3D RPG - Camera Relative Movement

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -10,8 +10,12 @@ public class PlayerStateMachine : StateMachine
 
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 
+    public Transform MainCameraTransform { get; private set; }
+
     private void Start()
     {
+        MainCameraTransform = Camera.main.transform;
+        
         SwitchState(new PlayerTestState(this));
     }
 }
