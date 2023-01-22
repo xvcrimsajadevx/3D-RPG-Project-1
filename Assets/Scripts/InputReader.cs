14c098ae1838e6c70cab7f3d1d2745454f6299fb
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-01-22 02:30:34 -0700

    3D RPG - Entering the Targeting State

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -10,6 +10,7 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
 
     public event Action JumpEvent;
     public event Action DodgeEvent;
+    public event Action TargetEvent;
 
     private Controls controls;
 
@@ -46,5 +47,11 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
     public void OnLook(InputAction.CallbackContext context)
     {
         
+    }
+
+    public void OnTarget(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }
+        TargetEvent?.Invoke();
     }
 }
