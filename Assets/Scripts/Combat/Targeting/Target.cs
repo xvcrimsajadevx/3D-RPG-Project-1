95070dfda932bdd025624bca7e086cc7d4058c23
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-01-28 13:02:07 -0700

    3D RPG - Cinemachine Target Group

--- Assets/Scripts/Combat/Targeting/Target.cs
+++ Assets/Scripts/Combat/Targeting/Target.cs
@@ -1,18 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public event Action<Target> OnDestroyed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDestroy() {
+        OnDestroyed?.Invoke(this);
     }
 }
