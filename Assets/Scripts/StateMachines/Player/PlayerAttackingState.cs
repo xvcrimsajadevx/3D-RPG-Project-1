10f4a7b4a85623475bb484ac0026adc3f515673b
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-01-31 05:19:24 -0700

    3D RPG - Combo Attacks

--- Assets/Scripts/StateMachines/Player/PlayerAttackingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerAttackingState.cs
@@ -1,27 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttackingState : PlayerBaseState
 {
+    private bool attackButtonPressed = false;
+    private float previousFrameTime;
     private Attack attack;
 
-    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackId) : base(stateMachine)
+    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackIndex) : base(stateMachine)
     {
-        attack = stateMachine.Attacks[attackId];
+        attack = stateMachine.Attacks[attackIndex];
     }
 
     public override void Enter()
     {
-        stateMachine.Animator.CrossFadeInFixedTime(attack.AnimationName, 0.1f);
+        stateMachine.InputReader.AttackEvent += OnAttackButtonPressed;
+
+        stateMachine.Animator.CrossFadeInFixedTime(attack.AnimationName, attack.TransitionDuration);
     }
 
     public override void Tick(float deltaTime)
     {
+        Move(deltaTime);
+
+        FaceTarget();
+
+        float normalizedTime = GetNormalizedTime();
+
+        if (normalizedTime > previousFrameTime && normalizedTime < 1f)
+        {
+            if (attackButtonPressed)
+            {
+                TryComboAttack(normalizedTime);
+
+                attackButtonPressed = false;
+            }
+        }
+        else
+        {
+            // Go back to locomotion
+        }
+
+        previousFrameTime = normalizedTime;
     }
 
     public override void Exit()
     {
+        stateMachine.InputReader.AttackEvent -= OnAttackButtonPressed;
+    }
+
+    private void TryComboAttack(float normalizedTime)
+    {
+        if (attack.ComboStateIndex == -1) { return; }
+        if (normalizedTime < attack.ComboAttackTime) { return; }
+
+        stateMachine.SwitchState
+        (
+            new PlayerAttackingState
+            (
+                stateMachine,
+                attack.ComboStateIndex
+            )
+        );
+    }
+
+    private float GetNormalizedTime()
+    {
+        AnimatorStateInfo currentInfo = stateMachine.Animator.GetCurrentAnimatorStateInfo(0);
+        AnimatorStateInfo nextInfo = stateMachine.Animator.GetNextAnimatorStateInfo(0);
+
+        if (stateMachine.Animator.IsInTransition(0) && nextInfo.IsTag("Attack"))
+        {
+            return nextInfo.normalizedTime;
+        }
+        else if (!stateMachine.Animator.IsInTransition(0) && currentInfo.IsTag("Attack"))
+        {
+            return currentInfo.normalizedTime;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
+    private void OnAttackButtonPressed()
+    {
+        if (!attackButtonPressed)
+        {
+            attackButtonPressed = true;
+        }
     }
 
 }
