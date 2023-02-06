742ffd35c65f76648168ccfebd81ec3e7c98d4c5
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-02-05 20:14:20 -0700

    3D RPG - Enemy Idle State

--- Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class EnemyStateMachine : StateMachine
 {
-    
+    [field: SerializeField] public Animator Animator { get; private set; }
+
+    private void Start()
+    {
+        SwitchState(new EnemyIdleState(this));
+    }
 }
