db2da777822c1102d329a760736f511eeb62fa93
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-02-07 22:02:24 -0700

    3D RPG - Enemy Attacking State

--- Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -12,6 +12,7 @@ public class EnemyStateMachine : StateMachine
 
     [field: SerializeField] public float MovementSpeed { get; private set; }
     [field: SerializeField] public float PlayerChaseRange { get; private set; }
+    [field: SerializeField] public float AttackRange { get; private set; }
 
     public GameObject Player { get; private set; }
 
