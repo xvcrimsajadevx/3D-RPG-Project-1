3204621957b17ee9fd9f00d6b6df15636cdd756d
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-02-10 03:08:36 -0700

    3D RPG - Dead States

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -10,6 +10,7 @@ public class Health : MonoBehaviour
     private int health;
 
     public event Action OnTakeDamage;
+    public event Action OnDie;
 
     private void Start()
     {
@@ -18,11 +19,16 @@ public class Health : MonoBehaviour
 
     public void DealDamage(int damage)
     {
-        if (health <= 0) { return; }
+        if (health == 0) { return; }
 
         health = Mathf.Max(health - damage, 0);
 
         OnTakeDamage?.Invoke();
+
+        if (health == 0)
+        {
+            OnDie?.Invoke();
+        }
 
         Debug.Log(health);
     }
