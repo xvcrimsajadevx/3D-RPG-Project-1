12964bd74634b82e798ec165830b854e6fc8357f
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-02-10 06:36:47 -0700

    3D RPG - Ragdoll on Death

--- Assets/Scripts/StateMachines/Enemy/EnemyDeadState.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyDeadState.cs
@@ -8,7 +8,7 @@ public class EnemyDeadState : EnemyBaseState
 
     public override void Enter()
     {
-        // Toggle ragdoll
+        stateMachine.Ragdoll.ToggleRagdoll(true);
         stateMachine.Weapon.gameObject.SetActive(false);
         GameObject.Destroy(stateMachine.Target);
     }
