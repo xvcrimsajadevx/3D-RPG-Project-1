dc67bd60cf19083a5bfe86b850296d706ed49b97
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-02-19 03:29:15 -0700

    3D RPG - Ledge Detector

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -12,6 +12,7 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public WeaponDamage Weapon { get; private set; }
     [field: SerializeField] public Health Health { get; private set; }
     [field: SerializeField] public Ragdoll Ragdoll { get; private set; }
+    [field: SerializeField] public LedgeDetector LedgeDetector { get; private set; }
 
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
     [field: SerializeField] public float TargetingMovementSpeed { get; private set; }
